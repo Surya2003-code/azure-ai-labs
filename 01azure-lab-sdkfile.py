67d318b88a7e2bbdf097c17bdab5ca3e91f38a07
Surya Narayan Sahu <suryanarayansahu2003@gmail.com>
2024-11-04 12:28:10 +0530

    Create 01azure-lab-sdkfile.py
    Signed-off-by: Surya Narayan Sahu <suryanarayansahu2003@gmail.com>

---
+++ 01azure-lab-sdkfile.py
@@ -0,0 +1,2 @@
+#This file contains essential environment variables required to connect to Azure AI services. By setting up these configurations, you can securely connect your applications to Azure AI without hardcoding sensitive information.
+#environment.
