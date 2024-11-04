6c55e12bd8c0682042881671f1ca041cc1f1dc1b
Surya Narayan Sahu <suryanarayansahu2003@gmail.com>
2024-11-04 12:29:07 +0530

    Update and rename 01azure-lab-sdkfile.py to 01Azure-lab-sdkfile.py
    Signed-off-by: Surya Narayan Sahu <suryanarayansahu2003@gmail.com>

--- 01azure-lab-sdkfile.py
+++
@@ -1,2 +0,0 @@
-#This file contains essential environment variables required to connect to Azure AI services. By setting up these configurations, you can securely connect your applications to Azure AI without hardcoding sensitive information.
-#environment.
